ce2b238371db27520beef77670d1f2f26253fbbe
ChrisTR1021 <christianridgell1021@gmail.com>
2024-02-10 18:44:48 -0500

    added read and write files

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -17,9 +17,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
+
+app.post("/api/notes", (req, res) => {
+    const body = JSON.stringify(req.body);
+    console.log(body);
+  
+    //new note and unique id
+    const { title, text } = req.body;
+    const newNote = {
+      title,
+      text,
+      id: cuid(),
+    }}); 
+
+
+
+    fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(notesList),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info("New note has been added successfully.")
+      );
+      console.log(notesList);
+  res.json(newNote);
+
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const notesList = JSON.parse(data);
+
+      notesList.push(newNote)
+    }});
