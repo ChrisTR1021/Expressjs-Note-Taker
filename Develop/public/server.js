79f138a0e8a09ca958d0f1656a4cc4b72c7e3c12
ChrisTR1021 <christianridgell1021@gmail.com>
2024-02-10 18:27:53 -0500

    added app initialization, sendfile and path

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -8,3 +8,18 @@ const cuid = require("cuid");
 const noteData = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
 
+// app initialization 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
