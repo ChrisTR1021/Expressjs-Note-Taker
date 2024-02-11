c5ca7d4d4334fdf2fb5082773ca76a7ed5e68b72
ChrisTR1021 <christianridgell1021@gmail.com>
2024-02-10 19:39:37 -0500

    reworked app get and sendfiles

--- server.js
+++ server.js
@@ -10,3 +10,20 @@ const newNotes = require('./db/db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
+app.get('.api/notes', (req, res) => {
+  res.json(newNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
